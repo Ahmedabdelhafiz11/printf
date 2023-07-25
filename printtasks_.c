426bb91db410ea1b00c8287e6fb87205f5013dbc
nezhataghy <nezhataghy@gmail.com>
2023-07-25 08:56:32 -0700

    printtask

---
+++ printtasks_.c
@@ -0,0 +1,24 @@
+/**
+ * print_u_number - Prints an unsigned number
+ * @n: unsigned integer as input
+ * Return: The amount of numbers printed
+ */
+int print_u_number(unsigned int n)
+{
+	int l, diviseur = 1;
+	unsigned int num = n;
+
+	l = 0;
+
+	while(num / diviseur > 9)
+		diviseur *= 10;
+
+	while(div != 0)
+	{
+		l += _putchar('0' + num / diviseur);
+		num %= diviseur;
+		diviseur /= 10;
+	}
+
+	return (l);
+}
