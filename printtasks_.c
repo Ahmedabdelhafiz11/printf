0e85d33f187a9c8b6747f2d56a3bb276fafd7a0d
nezhataghy <nezhataghy@gmail.com>
2023-07-25 16:28:27 -0700

    update

--- printtasks_.c
+++ printtasks_.c
@@ -25,3 +25,44 @@ int print_u_int(va_list in)
 
 	return (l);
 }
+/**
+ * print_octal - Prints a number in octal base
+ * @o: List args
+ * Return: Number of char printed
+ */
+
+int print_octal(va_list o)
+{
+	unsigned int len, powten, j, digit, n, num;
+	int count = 0;
+
+	n = va_arg(o, unsigned int);
+	if (n != 0)
+	{
+		num = n;
+		len = 0;
+		while (num != 0)
+		{
+			num /= 8;
+			len++;
+		}
+		powten = 1;
+		for (j = 1; j <= len - 1; j++)
+			powten *= 8;
+		for (j = 1; j <= len; j++)
+		{
+			digit = n / powten;
+			_putchar(digit + '0');
+			count++;
+			n -= digit * powten;
+			powten /= 8;
+		}
+	}
+	else
+	{
+		_putchar('0');
+		return (1);
+	}
+	return (count);
+}
+}
