467d6d48af17ff679e1b95a8121d63159287c622
nezhataghy <nezhataghy@gmail.com>
2023-07-25 05:58:33 -0700

    print func

--- printtasks.c
+++ printtasks.c
@@ -78,3 +78,29 @@ int print_int(va_list in)
 	_putchar(last + '0');
 	return (i);
 }
+
+/**
+ * print_bin - prints integer converted to bin
+ * @in: argument to print
+ * Return: number of characters printed
+*/
+
+int print_bin(va_list in)
+{
+	int count, i, n = va_arg(in, int);
+	int arr[31];
+
+	for (i = 0; i < 32; i++)
+		arr[i] = '\0';
+	for (i = 0; i < 32 && (n > 1); i++)
+	{
+		arr[i] = (n % 2);
+		n = (n / 2);
+	}
+	for (i = 31; i >= 0 && (arr[i] != '\0'); i--)
+	{
+		_printf("%d", arr[i]);
+		count++;
+	}
+return (count);
+}
