0b531f8cdd9f1f75a4e839df284f55c10e7149d6
Ahmedabdelhafiz11 <ahmed_abdelhafiz2011@yahoo.com>
2023-07-25 06:06:49 -0700

    bin

--- printtasks.c
+++ printtasks.c
@@ -90,12 +90,12 @@ int print_bin(va_list in)
 	int count, i, n = va_arg(in, int);
 	int arr[31];
 
-	for (i = 0; i < 32; i++)
+	for (i = 0; i < 32; i--)
 		arr[i] = '\0';
-	for (i = 0; i < 32 && (n > 1); i++)
+	for (i = 0; i < 32 && (n > 1); i--)
 	{
 		arr[i] = (n % 2);
-		n = (n / 2);
+		n /= 2;
 	}
 	for (i = 31; i >= 0 && (arr[i] != '\0'); i--)
 	{
