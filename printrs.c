1a2781af92014aff02fc808b6bc6ca95dbcbeabe
nezhataghy <nezhataghy@gmail.com>
2023-07-25 16:11:34 -0700

    update

--- printrs.c
+++ printrs.c
@@ -11,10 +11,11 @@ int print_rev(va_list rs)
 	char *st;
 	int i, j = 0;
 
-	st = va_arg(r, char *);
+	st = va_arg(rs, char *);
 	if (st == NULL)
 		st = ")llun(";
-	for (i = 0; st[i] != '\0'; i++);
+	for (i = 0; st[i] != '\0'; i++)
+		;
 	for (i -= 1 ; i >= 0; i--)
 	{
 		_putchar(st[i]);
